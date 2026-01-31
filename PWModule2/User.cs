f74f8e5ee2333a978aa9ba9696715269dd9963f4
dskmu <erasylbelenov@gmail.com>
2026-02-01 01:58:15 +0500

    Initial commit

---
+++ PWModule2/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWModule2
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        //public User(string name, string email, string role)
+        //{
+        //    Name = name;
+        //    Email = email;
+        //    Role = role;
+        //}
+    }
+}
